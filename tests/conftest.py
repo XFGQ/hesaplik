f815eefc8410a9d32187d6bf93339ed5f0fdb606
XFGQ <furkanduman1925@gmail.com>
2026-07-16 01:19:38 +0200

    fix(tests): .env yükle ve vscode yorumlayıcı ayarını ekle

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,13 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 import asyncpg
-import pytest
+from dotenv import load_dotenv
+
+load_dotenv()
 import pytest_asyncio
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy import text
